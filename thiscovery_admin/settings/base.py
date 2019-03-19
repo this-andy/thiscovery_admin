9da5a7aa4a9640c8d5706adececab9a21bd160b7
Andy P <amp205@medschl.cam.ac.uk>
2019-03-19 16:09:55 +0000

    Initial commit

--- thiscovery_admin/settings/base.py
+++ thiscovery_admin/settings/base.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-1mm!-*tu=vfkw$e47z--od)^s703ui%kn^i-xp0g(u+13hcva'
 
