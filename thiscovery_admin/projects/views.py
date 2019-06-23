cadbb02716965bc411b29e2e3ff46631d794f311
Andy P <amp205@medschl.cam.ac.uk>
2019-06-23 11:51:05 +0100

    #2689 comment: Hubspot functionality

--- thiscovery_admin/projects/views.py
+++ thiscovery_admin/projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .aws import get_secret, get_aws_namespace
 
-# Create your views here.
+
+def index(request):
+    # return HttpResponse('Authenticate HubSpot API')
+    hubspot_connection = get_secret('hubspot-connection')
+    api_key = hubspot_connection['api-key']
+    client_id = hubspot_connection['client-id']
+    context_dict = {'client_id': client_id, "namespace": get_aws_namespace()}
+    return render(request, 'hubspot.html', context=context_dict)
